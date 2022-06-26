d76dbb20e9de01a7037fb2fedd5bbc32836b6672
piotreknow02 <65082017+piotreknow02@users.noreply.github.com>
2022-06-26 23:20:35 +0200

    Config

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from './components/Navbar'
+
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <div className="container">
+        <h1>Video here</h1>
+      </div>
+    </>
+  )
+}
